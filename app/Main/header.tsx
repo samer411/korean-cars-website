a10f6a582597cf77d2522b02c8739639ca237450
Samer Nassim <74414236+samer411@users.noreply.github.com>
2024-12-22 11:54:33 +0200

    initial commit

---
+++ app/Main/header.tsx
@@ -0,0 +1,21 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="/public/i-platform_mobility 1.png" alt="logo"/>
+            <nav>
+            <a>알림마당</a>
+            <a>연구기관 소개</a>
+            <a>정보제공</a>
+            <a>정부사업공고</a>
+            <a>정부예산 지원사업</a>
+            <a>기술지원</a>
+        </nav>
+        <input ></input>
+        <div>
+            <a href="#">로그인</a>
+            <a href="#">회원가입</a>/
+        </div>
+        </header>
+        
+    )
+}
