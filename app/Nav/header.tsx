e8bc5c741f46526054037e673295baac900c289a
Samer Nassim <74414236+samer411@users.noreply.github.com>
2024-12-25 21:06:29 +0200

    initial commit

--- app/Nav/header.tsx
+++ app/Nav/header.tsx
@@ -3,20 +3,20 @@ import logo from '../../public/i-platform_mobility 1.png';
 
 export default function Header(){
     return(
-        <header className=''>
-            <Image
+        <header className='flex bg-nav_bar text-white p-8'>
+        <Image
       src={logo}
       width={50}
       height={50}
       alt="Picture of the author"
         />
-            <nav>
-            <a>알림마당</a>
-            <a>연구기관 소개</a>
-            <a>정보제공</a>
-            <a>정부사업공고</a>
-            <a>정부예산 지원사업</a>
-            <a>기술지원</a>
+        <nav>
+            <a className='px-3'>알림마당</a>
+            <a className='px-3'>연구기관 소개</a>
+            <a className='px-3'>정보제공</a>
+            <a className='px-3'>정부사업공고</a>
+            <a className='px-3'>정부예산 지원사업</a>
+            <a className='px-3'>기술지원</a>
         </nav>
         <input ></input>
         <div>
