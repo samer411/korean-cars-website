3a404b57ae1c79b63be595b87565c91360bd9161
Samer Nassim <74414236+samer411@users.noreply.github.com>
2024-12-29 12:01:00 +0200

    initial commit

---
+++ app/Nav/dropdowns/1_dropdown.tsx
@@ -0,0 +1,6 @@
+export default function dropdown_1(){
+    return(
+        <button className='px-3'>i-Platform Mobility</button>
+
+    )
+}
--- app/Nav/header.tsx
+++ app/Nav/header.tsx
@@ -1,27 +1,41 @@
 import Image from 'next/image';
 import logo from '../../public/i-platform_mobility 1.png';
-
+import search from '../../public/search-icon.svg';
+import dropdown_1 from './dropdowns/1_dropdown';
 export default function Header(){
     return(
-        <header className='flex bg-nav_bar text-white p-8'>
-        <Image
-      src={logo}
-      width={50}
-      height={50}
-      alt="Picture of the author"
+        <header className='flex items-center justify-between bg-nav_bar text-white pt-7 pb-5 text-xs'>
+            <a href="#" className='px-5'>
+             <Image
+                src={logo}
+                width={180}
+                alt="Picture of the author"
+                className='-mt-4'
         />
-        <nav>
-            <a className='px-3'>알림마당</a>
-            <a className='px-3'>연구기관 소개</a>
-            <a className='px-3'>정보제공</a>
-            <a className='px-3'>정부사업공고</a>
-            <a className='px-3'>정부예산 지원사업</a>
-            <a className='px-3'>기술지원</a>
+            </a>
+       
+        <nav className='pt-1'>
+            <dropdown_1></dropdown_1>
+            <button className='px-3'>알림마당</button>
+            <button className='px-3'>연구기관 소개</button>
+            <button className='px-3'>정보제공</button>
+            <button className='px-3'>정부사업공고</button>
+            <button className='px-3'>정부예산 지원사업</button>
+            <button className='px-3'>기술지원</button>
         </nav>
-        <input ></input>
-        <div>
-            <a href="#">로그인</a>
-            <a href="#">회원가입</a>/
+        <div className='relative'>
+
+        <input className='ml-6 py-3 px-5 rounded-md placeholder-white text-white bg-gray-700 focus:outline-none w-80'placeholder='검색어를 입력하세요.'></input>
+        <Image 
+        src={search} 
+        width={20}
+         alt="search" 
+         className="absolute right-3 top-2"/>
+        </div>
+        <div className='px-8'>
+            <a className ='px-5'href="#">로그인</a>
+            <span>|</span>
+            <a className ='px-5' href="#">회원가입</a>
         </div>
         </header>
         
